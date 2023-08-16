014e06049f4dde69b42f92d98cc6e72fc3743069
Poomathi Subramani <poomathi@mallow-tech.com>
2023-08-16 13:27:21 +0530

    no message

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+    <div class="container-fluid">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link active" href="#">Blogs</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" href="#">Home</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Categories</a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">News</a></li>
+                    <li><a class="dropdown-item" href="#">Entertainment</a></li>
+                    <li><a class="dropdown-item" href="#">Sports</a></li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" href="#">NewsLetter</a>
+            </li>
+        </ul>
+        <form class="d-flex">
+            <input class="form-control me-2" type="text" placeholder="Search">
+            <button class="btn btn-outline-light" type="button">Search</button>
+        </form>
+    </div>
+</nav>
