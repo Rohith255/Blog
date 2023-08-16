649e0784d7fe02f25cac8878afcdd40eb5154f46
Poomathi Subramani <poomathi@mallow-tech.com>
2023-08-16 13:24:58 +0530

    separated navbar from index file

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,36 +16,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
-    <div class="container-fluid">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link active" href="#">Blogs</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link active" href="#">Home</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Categories</a>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">News</a></li>
-                    <li><a class="dropdown-item" href="#">Entertainment</a></li>
-                    <li><a class="dropdown-item" href="#">Sports</a></li>
-                </ul>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link active" href="#">NewsLetter</a>
-            </li>
-        </ul>
-        <form class="d-flex">
-            <input class="form-control me-2" type="text" placeholder="Search">
-            <button class="btn btn-outline-light" type="button">Search</button>
-        </form>
-    </div>
-</nav>
-<div class="container-fluid">
     @yield('content')
-</div>
-
 </body>
 </html>
